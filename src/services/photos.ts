202c6d9ded059af85322b1c3a50eb17b76c53528
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-12-18 00:08:38 -0300

    feat: add insert function

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,8 +1,9 @@
 import { Photo } from '../types/photo'
 import { storage } from '../config/firebase'
-import { ref, listAll, getDownloadURL } from 'firebase/storage'
+import { ref, listAll, getDownloadURL, uploadBytes } from 'firebase/storage'
+import { v4 as createId } from 'uuid'
 
-const getAll = async () => {
+export const getAll = async () => {
   const list: Photo[] = []
 
   const imagesFolder = ref(storage, 'images')
@@ -19,4 +20,17 @@ const getAll = async () => {
   return list
 }
 
-export default getAll
+export const insert = async (file: File) => {
+  if (['image/jpeg', 'image/jpg', 'image/png'].includes(file.type)) {
+    const randomName = createId()
+    const newFile = ref(storage, `images/${randomName}')`)
+    const upload = await uploadBytes(newFile, file)
+    const photoUrl = await getDownloadURL(upload.ref)
+    return {
+      name: upload.ref.name,
+      url: photoUrl
+    } as Photo
+  } else {
+    return new Error('Tipo de arquivo não permitido.')
+  }
+}
