56146cd21121b6774e863e93b153391fc680ea8e
Cleiton Sobrinho <cleiton1.life@gmail.com>
2022-12-27 10:07:05 -0300

    fix: add change delete file

--- src/components/photo-item/photo-item.component.tsx
+++ src/components/photo-item/photo-item.component.tsx
@@ -1,19 +1,36 @@
 import * as C from './photo-item.styles'
 import { RiDeleteBin6Line } from 'react-icons/ri'
-import * as Photos from '../../services/photos'
+import { deleteObject, getStorage, ref } from 'firebase/storage'
+import { useContext } from 'react'
+import { PhotosContext } from '../../contexts/context'
 
 type Props = {
     url: string
     name: string | number
 }
+
+type PropsName = {
+  name: string | number
+}
 const PhotoItem = ({ url, name }: Props) => {
+  const { getPhotos } = useContext(PhotosContext)
+  const deleteFile = async ({ name }:PropsName) => {
+    const storage = getStorage()
+    const desertRef = ref(storage, `images/${name}`)
+    deleteObject(desertRef).then(() => {
+      getPhotos()
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   return (
         <C.Container>
             <C.ImgContainer>
                  <img src={url}/>
             </C.ImgContainer>
             <C.ButtonContainer>
-                 <button onClick={() => Photos.deleteFile({ name })}><RiDeleteBin6Line size={20} /></button>
+                 <button onClick={() => deleteFile({ name })}><RiDeleteBin6Line size={20} /></button>
             </C.ButtonContainer>
 
         </C.Container>
